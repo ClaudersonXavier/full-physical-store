9c9917f9142be5ec086c742e45e1a1da63d938bf
Clauderson Branco Xavier <xavierclauderson98@gmail.com>
2025-01-04 16:03:19 -0300

    Estabelecendo conexão com o banco de dados do mongoDB

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { StoreController } from './store.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  controllers: [StoreController]
+  controllers: [StoreController],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE)
+  ]
 })
 export class StoreModule {}
